c97816152bb65cb7a0469a33c300777fc68206d3
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-12-08 17:36:37 +0100

    update

--- Pages/Admin.razor.cs
+++ Pages/Admin.razor.cs
@@ -1,6 +1,21 @@
-﻿namespace TORCHAIN.Pages
+﻿using System.Runtime.CompilerServices;
+
+using Microsoft.AspNetCore.Components;
+using TORCHAIN.Repositories;
+
+namespace TORCHAIN.Pages
 {
     public partial class Admin
     {
+        [Inject]
+        private IForumRepository? _repository { get; set; }
+        [Inject]
+        public NavigationManager? NavigationManager { get; set; }
+
+        public IEnumerable<PostEntity>? Posts { get; set; }
+        protected async override Task OnInitializedAsync()
+        {
+            Posts = await _repository!.GetAllPosts();
+        }
     }
 }
