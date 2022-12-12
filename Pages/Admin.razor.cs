0056f8de0445628cd93e0ade315be70d0491a99a
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-12-12 09:40:54 +0100

    update

--- Pages/Admin.razor.cs
+++ Pages/Admin.razor.cs
@@ -16,12 +16,48 @@ namespace TORCHAIN.Pages
         public IEnumerable<CommentEntity>? Comments { get; set; }
         public IEnumerable<CategoryEntity>? Categories { get; set; }
         public IEnumerable<DarknetGalleryEntity>? Gallery { get; set; }
+        public PostEntity Post { get; set; } = new PostEntity();
+        public CommentEntity Comment { get; set; } = new CommentEntity();
+        public CategoryEntity Category { get; set; } = new CategoryEntity();
+        public DarknetGalleryEntity GalleryItem { get; set; } = new DarknetGalleryEntity();
         protected async override Task OnInitializedAsync()
         {
             Posts = await _repository!.GetAllPosts();
             Comments = await _repository!.GetAllComments();
             Categories = await _repository!.GetAllCategories(isVerified: false);
             Gallery = await _repository!.GetAllImages(isVerified: false);
+        }
+        private async Task AcceptPost(int id)
+        {
+          
+        }
+        private async Task DenyPost(int id)
+        {
+
+        }
+        private async Task AcceptComment(int id)
+        {
+            
+        }
+        private async Task DenyComment(int id)
+        {
+
+        }
+        private async Task AcceptCategory(int id)
+        {
+
+        }
+        private async Task DenyCategory(int id)
+        {
+            
+        }
+        private async Task AcceptGalleryItem(int id)
+        {
+
+        }
+        private async Task DenyGalleryItem(int id)
+        {
+
         }
     }
 }
