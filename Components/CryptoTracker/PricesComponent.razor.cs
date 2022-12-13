dedff0caa9e8a56ffe39600fbf821a5d406a4755
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-12-13 18:20:16 +0100

    update

---
+++ Components/CryptoTracker/PricesComponent.razor.cs
@@ -0,0 +1,34 @@
+﻿using Binance.Spot;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using TORCHAIN.Models;
+
+namespace TORCHAIN.Components.CryptoTracker
+{
+    public partial class PricesComponent
+    {
+        private static HttpClient _httpClient = new HttpClient();
+        public List<string> Lista = new List<string>();
+        protected async override Task OnInitializedAsync()
+        {
+            #region BinanceAPI
+            using (_httpClient = new HttpClient())
+            {
+                _httpClient.BaseAddress = new Uri("https://testnet.binance.vision");
+                _httpClient.Timeout = new TimeSpan(0, 0, 5);
+                _httpClient.DefaultRequestHeaders.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
+                _httpClient.DefaultRequestHeaders.Add("X-MBX-APIKEY", Environment.GetEnvironmentVariable("APIKEY"));
+                _httpClient.DefaultRequestHeaders.Add("SecretKey", Environment.GetEnvironmentVariable("SECRETKEY"));
+                var symbol = "BTCBUSD";
+                var market = new Market(_httpClient);
+                var result = await market.CurrentAveragePrice(symbol);
+                BinancePrice? Token = JsonConvert.DeserializeObject<BinancePrice>(result);
+                Lista.Add(Token!.Price.ToString());
+            }
+            #endregion
+
+            
+        }
+    }
+}
