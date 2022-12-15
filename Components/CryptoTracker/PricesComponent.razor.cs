fa1432f68535afd4624be1952fa898cfa662f0fb
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-12-15 14:01:10 +0100

    update

--- Components/CryptoTracker/PricesComponent.razor.cs
+++ Components/CryptoTracker/PricesComponent.razor.cs
@@ -9,10 +9,56 @@ namespace TORCHAIN.Components.CryptoTracker
     public partial class PricesComponent
     {
        private static HttpClient _httpClient = new HttpClient();
-        public Dictionary<string, string> Lista = new Dictionary<string, string>();
+        public Dictionary<string, string> Lista = new Dictionary<string, string>()
+        {
+            {"Binance","0" },
+            {"Zonda","0" }
+        };
+        public string BinancePrice { get; set; } 
+        public string ZondaPrice { get; set; } 
         protected async override Task OnInitializedAsync()
         {
+
             #region BinanceAPI
+
+            #endregion
+
+            #region Zonda
+            try
+                {
+                    using (_httpClient = new HttpClient())
+                    {
+                        _httpClient.BaseAddress = new Uri("https://api.zonda.exchange");
+                        _httpClient.Timeout = new TimeSpan(0, 0, 5);
+                        _httpClient.DefaultRequestHeaders.Clear();
+                        var response = await _httpClient.GetAsync("/rest/trading/ticker/BTC-USDT");
+                        response.EnsureSuccessStatusCode();
+                        var body = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(body);
+                        ZondaPrice? zondaPrice = JsonConvert.DeserializeObject<ZondaPrice>(body);
+                    Lista["Zonda"] = zondaPrice!.ticker.rate;
+                }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.InnerException?.Message ?? exception.Message);
+                }
+                #endregion
+
+                #region Bitrue
+                try
+                {
+
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.InnerException?.Message ?? exception.Message);
+                }
+            #endregion
+
+        }
+        private async void PriceCheck()
+        {
             try
             {
                 using (_httpClient = new HttpClient())
@@ -27,16 +73,14 @@ namespace TORCHAIN.Components.CryptoTracker
                     var market = new Market(_httpClient);
                     var result = await market.CurrentAveragePrice(symbol);
                     BinancePrice? binancePrice = JsonConvert.DeserializeObject<BinancePrice>(result);
-                    Lista.Add("Binance", binancePrice!.Price.ToString());
+                    Lista["Binance"] = binancePrice!.Price.ToString();
+                    BinancePrice = binancePrice!.Price.ToString();
                 }
             }
             catch (BinanceClientException binanceException)
             {
                 Console.WriteLine(binanceException.InnerException?.Message ?? binanceException.Message);
             }
-            #endregion
-
-            #region Zonda
             try
             {
                 using (_httpClient = new HttpClient())
@@ -49,14 +93,15 @@ namespace TORCHAIN.Components.CryptoTracker
                     var body = await response.Content.ReadAsStringAsync();
                     Console.WriteLine(body);
                     ZondaPrice? zondaPrice = JsonConvert.DeserializeObject<ZondaPrice>(body);
-                    Lista.Add("Zonda", zondaPrice.ticker.rate);
+                    Lista["Zonda"] = zondaPrice!.ticker.rate;
+                    ZondaPrice = zondaPrice!.ticker.rate;
                 }
             }
-            catch(Exception exception)
+            catch (Exception exception)
             {
                 Console.WriteLine(exception.InnerException?.Message ?? exception.Message);
             }
-            #endregion
+
 
         }
     }
